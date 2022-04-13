0d8785d93c42a16e01a2bc339d63fe44a9f3ac68
trian-gles <69212477+trian-gles@users.noreply.github.com>
2022-04-13 10:57:54 -0400

    multiple tables per sonic area

---
+++ javascripts/std-dev.js
@@ -0,0 +1,33 @@
+inlets = 1;
+outlets = 1;
+
+function randn_bm() {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+
+function msg_float(f) {
+	let args = Array.from(jsarguments);
+	
+	outlet(0, distributed_choice(f, args[1], args[2]));
+}
+
+function distributed_choice(center, low, high)
+{
+	let rand = low - 1;
+	while (rand < low || rand > high)
+	{
+		rand = ((randn_bm() - 0.5) * 10) + center;
+	}
+	return rand;
+}
+
+function list()
+{
+	let args = Array.from(arguments);
+	let bounds = [args[1], args[2]];
+	let rand = randn_bm() * (bounds[1] - bounds[0]) + bounds[0];
+	outlet(0, rand);
+}
